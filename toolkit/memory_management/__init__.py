7bb26c82ccc30c6fb0352268eee28ce574f179d4
rmatif <rmatif@proton.me>
2025-12-15 12:31:52 +0000

    robust VRAM freeing

--- toolkit/memory_management/__init__.py
+++ toolkit/memory_management/__init__.py
@@ -1,2 +1,4 @@
 from .manager import MemoryManager
-from .manager_modules import sync_grad_transfers
+from .manager_modules import clear_device_state, sync_grad_transfers
+
+__all__ = ["MemoryManager", "clear_device_state", "sync_grad_transfers"]
